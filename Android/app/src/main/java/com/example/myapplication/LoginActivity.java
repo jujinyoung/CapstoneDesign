5de751bc490c96caa2bbc9bbe7dd9e31255dfe9c
jujinyoung <jujinyoung1838@naver.com>
2021-10-06 23:08:20 +0900

    로그인
    로그인 액티비티, 회원가입 액티비티 생성
    로그인 기능 완성

---
+++ Android/app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -0,0 +1,89 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText et_id, et_pass;    //아이디,패스워드 텍스트
+    private Button btn_login,btn_register;  //로그인,회원가입 버튼
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        et_id = findViewById(R.id.et_id);
+        et_pass = findViewById(R.id.et_pass);
+        btn_login = findViewById(R.id.btn_login);
+        btn_register = findViewById(R.id.btn_register);
+
+        //region 로그인
+
+        //로그인 버튼 클릭
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //id,password 값저장
+                String userID = et_id.getText().toString();
+                String userPass = et_pass.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            //서버에 로그인 요청을 하면 결과값을 json타입으로 받음
+                            JSONObject jsonObject = new JSONObject(response);
+                            //서버통신 성공여부
+                            boolean success = jsonObject.getBoolean("success");
+                            if (success) {
+//                                String userID = jsonObject.getString("userID");
+//                                String userPass = jsonObject.getString("userPassword");
+                                Toast.makeText(getApplicationContext(), "로그인에 성공하였습니다", Toast.LENGTH_SHORT).show();
+                                //메인화면으로 이동
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                            }else{
+                                Toast.makeText(getApplicationContext(),"로그인에 실패하였습니다",Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                //vollyer를 이용해서 서버에 요청
+                LoginRequest loginRequest = new LoginRequest(userID,userPass,responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
+            }
+        });
+        //endregion
+
+        //region 회원가입
+
+        //회원가입 버튼 클릭
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //회원가입 화면으로 이동
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+        //endregion
+    }
+}
---
+++ Android/app/src/main/java/com/example/myapplication/LoginRequest.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginRequest extends StringRequest {
+
+    //서버 URl 설정
+    final static private String URL = "라즈베리파이ip주소/로그인js파일";
+    private Map<String,String> map;
+
+    public LoginRequest(String userID, String userPassword, Response.Listener<String> listener){
+        super(Method.POST, URL, listener, null);
+
+        map = new HashMap<>();
+        map.put("userID",userID);
+        map.put("userPassword",userPassword);
+    }
+
+    //서버에서 요청하는 파라미터 전송
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
+}
