28eb0bbc8931819d4e4aede8966c929651cd90cd
jujinyoung <jujinyoung1838@naver.com>
2021-11-15 23:46:16 +0900

    카메라 기능 추가중

--- Android/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Android/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,127 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.pedro.library.AutoPermissions;
+import com.pedro.library.AutoPermissionsListener;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements AutoPermissionsListener{
+
+    ImageButton cameraBtn;
+    File file;
+    Uri fileUri;
+    Bitmap rotatedBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AutoPermissions.Companion.loadAllPermissions(this, 101);
+
+        cameraBtn = findViewById(R.id.cameraBtn);
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TakePicture();
+            }
+        });
+    }
+
+    private void TakePicture() {
+        try {
+            if (file == null) {
+                file = createFile();
+            }else{
+                file.delete();
+            }
+
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if(Build.VERSION.SDK_INT >= 24) {
+            fileUri = FileProvider.getUriForFile(this, "org.techtown.test.fileprovider", file);
+        }else{
+            fileUri = Uri.fromFile(file);
+        }
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,fileUri);
+        if(intent.resolveActivity((getPackageManager())) != null){
+            startActivityForResult(intent, 101);
+        }
+    }
+
+    private File createFile() {
+        String filename = "capture.jpg";
+
+        File storageDir = Environment.getExternalStorageDirectory();
+        File outFile = new File(storageDir,filename);
+        return outFile;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+            case 101:
+                if(resultCode==RESULT_OK) {
+                    BitmapFactory.Options options = new BitmapFactory.Options();
+                    Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
+                    if (bitmap == null) {
+                        Toast.makeText(this, "할당 안됨", Toast.LENGTH_LONG).show();
+                    } else {
+                        rotatedBitmap = rotateImage(bitmap);
+                        cameraBtn.setImageBitmap(rotatedBitmap);
+                    }
+                }
+                break;
+        }
+    }
+
+    //이미지 회전
+    public static Bitmap rotateImage(Bitmap source){
+        Matrix matrix = new Matrix();
+        matrix.postRotate(90);
+        return Bitmap.createBitmap(source,0,0,source.getWidth(),source.getHeight(),matrix,true);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        AutoPermissions.Companion.parsePermissions(this,requestCode,permissions,this);
+    }
+
+    @Override
+    public void onDenied(int i, String[] permissions) {
+        Toast.makeText(this, "permissions denied : " + permissions.length, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onGranted(int i, String[] permissions) {
+        Toast.makeText(this,"permissions granted : " + permissions.length, Toast.LENGTH_LONG).show();
     }
 }
