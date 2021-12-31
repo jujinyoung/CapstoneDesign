5012bfd53acc94ac2156f5cb8d9bd39a0c2e4a08
jujinyoung <jujinyoung1838@naver.com>
2021-12-31 21:48:11 +0900

    카메라 저장 에러 해결
    이미지 살짝 짤림

--- Android/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Android/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -14,6 +14,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -36,7 +37,6 @@ public class MainActivity extends AppCompatActivity implements AutoPermissionsLi
     ImageButton cameraBtn;
     File file;
     Uri fileUri;
-    Bitmap rotatedBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,20 +68,26 @@ public class MainActivity extends AppCompatActivity implements AutoPermissionsLi
         }
 
         if(Build.VERSION.SDK_INT >= 24) {
-            fileUri = FileProvider.getUriForFile(this, "org.techtown.test.fileprovider", file);
+            fileUri = FileProvider.getUriForFile(this, "org.techtown.caps.fileprovider", file);
         }else{
             fileUri = Uri.fromFile(file);
         }
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         intent.putExtra(MediaStore.EXTRA_OUTPUT,fileUri);
+
         startActivityResult.launch(intent);
     }
 
     private File createFile() {
         String filename = "capture.jpg";
 
-        File storageDir = Environment.getExternalStorageDirectory();
-        File outFile = new File(storageDir,filename);
+        File storageDir = getExternalCacheDir();
+        File outFile = null;
+        try {
+            outFile = File.createTempFile("android_upload", ".jpg", storageDir);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return outFile;
     }
 
@@ -93,42 +99,72 @@ public class MainActivity extends AppCompatActivity implements AutoPermissionsLi
                     if(result.getResultCode() == Activity.RESULT_OK){
                         BitmapFactory.Options options = new BitmapFactory.Options();
                         Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
+
+                        ExifInterface exif = null;
+                        try {
+                            exif = new ExifInterface(file.getAbsolutePath());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        int exifOrientation;
+                        int exifDegree;
+
+                        if (exif != null) {
+                            exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+                            exifDegree = exifOrientationToDegrees(exifOrientation);
+                        } else {
+                            exifDegree = 0;
+                        }
+
                         if (bitmap == null) {
                             Toast.makeText(getApplicationContext(), "할당 안됨", Toast.LENGTH_LONG).show();
                         } else {
-                            rotatedBitmap = rotateImage(bitmap);
-                            cameraBtn.setImageBitmap(rotatedBitmap);
+                            Bitmap resizeBitmap = resizeImage(rotateImage(bitmap,exifDegree));
+                            cameraBtn.setImageBitmap(resizeBitmap);
                         }
                     }
                 }
             }
     );
 
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        switch (requestCode){
-//            case 101:
-//                if(resultCode==RESULT_OK) {
-//                    BitmapFactory.Options options = new BitmapFactory.Options();
-//                    Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
-//                    if (bitmap == null) {
-//                        Toast.makeText(this, "할당 안됨", Toast.LENGTH_LONG).show();
-//                    } else {
-//                        rotatedBitmap = rotateImage(bitmap);
-//                        cameraBtn.setImageBitmap(rotatedBitmap);
-//                    }
-//                }
-//                break;
-//        }
-//    }
-
+    private int exifOrientationToDegrees(int exifOrientation) {
+        if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
+            return 90;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
+            return 180;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
+            return 270;
+        }
+        return 0;
+    }
     //이미지 회전
-    public static Bitmap rotateImage(Bitmap source){
+    private Bitmap rotateImage(Bitmap bitmap, float degree) {
         Matrix matrix = new Matrix();
-        matrix.postRotate(90);
-        return Bitmap.createBitmap(source,0,0,source.getWidth(),source.getHeight(),matrix,true);
+        matrix.postRotate(degree);
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+    }
+
+    private Bitmap resizeImage(Bitmap bitmap){
+        int width = cameraBtn.getWidth();
+        int height = cameraBtn.getHeight();
+        float bmpWidth = bitmap.getWidth();
+        float bmpHeight = bitmap.getHeight();
+
+        if (bmpWidth > width) {
+            // [원하는 너비보다 클 경우의 설정]
+            float mWidth = bmpWidth / 100;
+            float scale = width/ mWidth;
+            bmpWidth *= (scale / 100);
+            bmpHeight *= (scale / 100);
+        } else if (bmpHeight > height) {
+            // [원하는 높이보다 클 경우의 설정]
+            float mHeight = bmpHeight / 100;
+            float scale = height/ mHeight;
+            bmpWidth *= (scale / 100);
+            bmpHeight *= (scale / 100);
+        }
+
+        return  Bitmap.createScaledBitmap(bitmap, (int) bmpWidth, (int) bmpHeight, true);
     }
 
     @Override
@@ -137,6 +173,7 @@ public class MainActivity extends AppCompatActivity implements AutoPermissionsLi
 
         AutoPermissions.Companion.parsePermissions(this,requestCode,permissions,this);
     }
+
 
     @Override
     public void onDenied(int i, String[] permissions) {
