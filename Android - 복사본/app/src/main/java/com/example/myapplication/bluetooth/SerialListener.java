5357f4f6209fc52553495dc961b7fb1941176cd0
jujinyoung <jujinyoung1838@naver.com>
2022-06-04 23:46:15 +0900

    .

--- Android - 복사본/app/src/main/java/com/example/myapplication/bluetooth/SerialListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.bluetooth;
-
-interface SerialListener {
-    void onSerialConnect      ();
-    void onSerialConnectError (Exception e);
-    void onSerialRead         (byte[] data);
-    void onSerialIoError      (Exception e);
-}
-
